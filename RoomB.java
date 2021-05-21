653e8dc832211eaa4e915c1a17aea566e5da8a70
DevNam2k1 <dinhnamsaker@gmail.com>
2021-05-21 17:25:36 +0700

    Hotel manager

---
+++ RoomB.java
@@ -0,0 +1,15 @@
+
+public class RoomB extends Room {
+	 public RoomB() {
+	        super("A", 500);
+	    }
+
+	    @Override
+	    public String toString() {
+	        return "RoomA{" +
+	                "category='" + category + '\'' +
+	                ", price=" + price +
+	                '}';
+	    }
+
+}
---
+++ RoomC.java
@@ -0,0 +1,15 @@
+
+public class RoomC extends Room{
+	 public RoomC() {
+	        super("A", 500);
+	    }
+
+	    @Override
+	    public String toString() {
+	        return "RoomA{" +
+	                "category='" + category + '\'' +
+	                ", price=" + price +
+	                '}';
+	    }
+
+}
